7c43ad2693377fc4727c832986ee9687c83f7a9d
Krishna <krishn2014@gmail.com>
2016-10-27 23:50:22 -0700

    provisioned scheme details api

---
+++ wsgi/project/mfapp/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+# importing models
+from models import *
+
+class SchemeDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Scheme
+        fields = ['scheme_code', 'isin_payout_growth', 'isin_div_reinvestment', 'scheme_name', 'net_asset_value', 'repurchase_price', 'sale_price', 'date']
--- wsgi/project/mfapp/views.py
+++ wsgi/project/mfapp/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse, Http404
+from django.http import HttpResponseRedirect
+
+# importing models
+from models import *
+
 # Create your views here.
+
+# from django.utils import timezone
+# from django.contrib.auth.models import User, Group
+
+# Django REST framework
+from serializers import *
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class SchemeDetail(APIView):
+    """
+    Retrieve details of a schema.
+    """
+    def get_object(self, schemecode):
+        try:
+            return Scheme.objects.get(scheme_code=schemecode)
+        except Scheme.DoesNotExist:
+            raise Http404
+
+    def get(self, request, schemecode, format=None):
+        scheme = self.get_object(schemecode)
+        serializer = SchemeDetailsSerializer(scheme)
+        return Response(serializer.data)
